cb1c5ccac3c6f8296c8242f9b72a41a38b3e9bbd
ndutagrace25 <ndutagrace25@gmail.com>
2024-01-06 14:00:34 +0300

    added another solution for leetcode 217

--- leetCode217.js
+++ leetCode217.js
@@ -23,26 +23,44 @@ Constraints:
 -109 <= nums[i] <= 109
 */
 
-var containsDuplicate = function (nums) {
-  let distinct = [...new Set(nums)];
-  if (distinct.length === nums.length) {
-    return false;
-  }
+// var containsDuplicate = function (nums) {
+//   let distinct = [...new Set(nums)];
+//   if (distinct.length === nums.length) {
+//     return false;
+//   }
 
-  let left = 0;
-  let right = nums.length - 1;
-  let result = false;
+//   let left = 0;
+//   let right = nums.length - 1;
+//   let result = false;
 
-  while (left < nums.length) {
-    if (nums[left] === nums[right]) {
-      result = true;
+//   while (left < nums.length) {
+//     if (nums[left] === nums[right]) {
+//       result = true;
+//     }
+
+//     left++;
+//   }
+
+//   return result;
+// };
+
+//2
+
+const containsDuplicate = (nums) => {
+  let visitedNums = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+
+    if (visitedNums[num]) {
+      return true;
+    } else {
+      visitedNums[num] = true;
     }
-
-    left++;
   }
 
-  return result;
+  return false;
 };
-console.log(containsDuplicate([1,2,3,1]));
-console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
 console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
